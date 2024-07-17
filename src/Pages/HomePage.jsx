824230c2eab6243f287f44c066c91669f5286bd2
Abel Bright <oluwasegunbarnabas@gmail.com>
2024-07-17 14:27:31 +0100

    remove the cart page

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import { NavBar, Loading } from '../Components';
+import { NavBar } from '../Components';
 
 const HomePage = () => {
   return (
     <main className=" bg-[#f1f1f2] min-h-screen flex flex-col justify-between  relative">
       <NavBar />
-      <Loading />
+
       <div className="align-el relative mt-2 lg:mt-20">
         <Outlet />
       </div>
